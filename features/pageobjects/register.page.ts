7ba8ff1eef7001711550f4bc9a94cc014ea01447
Chaudhari <mukul-rajendra.chaudhari@capgemini.com>
2021-12-16 12:17:21 +0530

    Code optimised with few changes in feature and spec file

--- features/pageobjects/register.page.ts
+++ features/pageobjects/register.page.ts
@@ -3,7 +3,6 @@ class Registration {
     get registrationHeading() { return $('//h1[text()="Registration"]') }
     get clickRegistrationTab() { return $('//img[@src="images/registration.jpg"]') }
     get registrationFormHeading() { return $('//h2[text()="Registration Form"]') }
-
     get firstName() { return $('//form[@id="register_form"]/fieldset[1]/p[1]/input') }
     get lastName() { return $('//form[@id="register_form"]/fieldset[1]/p[2]/input') }
     get matrialStatus() { return $('//form[@id="register_form"]/fieldset[2]//label[1]/input') }
@@ -11,6 +10,7 @@ class Registration {
     get phoneNumber() { return $('//form[@class="form_box"]/fieldset[6]/input') }
     get username() { return $('//form[@class="form_box"]/fieldset[7]/input') }
     get eMail() { return $('//form[@class="form_box"]/fieldset[8]/input') }
+    get fileUpload() { return $('//input[@type="file"]') }
     get aboutYourself() { return $('//form[@class="form_box"]/fieldset[10]/textarea') }
     get password() { return $('//form[@class="form_box"]/fieldset[11]/input') }
     get confirmPassword() { return $('//form[@class="form_box"]/fieldset[12]/input') }
